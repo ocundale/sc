3fb79ee64e9679b4dca87de4a70717540326ba9f
Oli Cundale <ocundale@gmail.com>
2014-08-22 18:03:59 +0100

    latest

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,28 +36,34 @@ scApp.factory('followerData', function($http) {
       var username = scDetails.username || 'deepervibrations',
           offset = scDetails.offset || '1',
           limit = scDetails.tracksPerArtist || '10',
-          days = scDetails.daysOld ||3,
           client_id = 'XXXX';
 
       return $http.jsonp('http://api.soundcloud.com/users/' + username + '/followings.json', {
         params: {
           "client_id": client_id,
           "limit":limit,
-          "callback": "JSON_CALLBACK",
-          "offset": offset
+          "offset": offset,
+          "callback": "JSON_CALLBACK"
         }
       });
     };
 
-    factory.trackSearch = function(followerUsername) {
+    return factory;
+});
+
+// Get List of followers of USERNAME
+scApp.factory('trackData', function($http) {
+  var factory = {};
+
+    factory.trackSearch = function(artist) {
         //return the promise directly.
         var limit = '1',
         client_id = 'XXXX';
-        return $http.jsonp('http://api.soundcloud.com/users/' + followerUsername + '/tracks.json', {
+        return $http.jsonp('http://api.soundcloud.com/users/' + artist + '/tracks.json', {
           params: {
             "client_id": client_id,
-            "callback": "JSON_CALLBACK",
-            "limit":limit
+            "limit":limit,
+            "callback": "JSON_CALLBACK"
           }
         });
       };
@@ -65,18 +71,19 @@ scApp.factory('followerData', function($http) {
     return factory;
 });
 
-
- scApp.controller('followerSearch', function($scope, $location, $routeParams, followerData, $sce) {
+scApp.controller('followerSearch', function($scope, $location, $routeParams, followerData, trackData, $sce) {
   $scope.search = function() {
-    $scope.data = null;
+    $scope.tracks = null;
     $scope.loading = true;
+    $scope.loading2 = true;
     followerData.userSearch($scope.scDetails)
     .then(function success(followers){
         var track,
+            artist,
             tracks = [];
         followers.data.forEach(function(follower) {
-          console.log('loading data for: ',follower.permalink);
-          followerData.trackSearch(follower.permalink)
+          artist = follower.permalink;
+          trackData.trackSearch(artist)
           .success(function success(trackobj) {
             track = trackobj[0];
               track.src = $sce.trustAsResourceUrl('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + track.id + '&amp;auto_play=false&amp;color=ff5500&amp;inverse=false&amp;show_user=true');
@@ -85,6 +92,7 @@ scApp.factory('followerData', function($http) {
               $scope.tracks = tracks;
           });
         });
+        $scope.loading2 = false;
     }, function (response) {
       alert("check yo' username FOO!");
       // error handler
@@ -98,16 +106,12 @@ scApp.factory('followerData', function($http) {
   //  $scope.search();
   // }
 });
-var hideLoading = function() {
-  document.getElementById('divLoading').style.display = "none";
-             document.getElementById('divFrameHolder').style.display = "block";
-};
 
 scApp.directive('scembed', function () {
   return {
     restrict: 'E',
     replace: true,
-    template: '<iframe width="100%" id="divLoading" height="20" scrolling="no" frameborder="no" onload="hideLoading()"></iframe>'
+    template: '<iframe width="100%" height="20" scrolling="no" frameborder="no"></iframe>'
     // link: function (scope, element, attributes) {
 
     // }
