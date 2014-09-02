cc0bd63f30588fc7414b4a0f4c645a9b23a3c4d1
Oli Cundale <ocundale@gmail.com>
2014-09-02 12:50:55 +0100

    working version ready for test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,11 @@
  * Main module of the application.
  */
 
- var scApp = angular.module('soondcloodApp', ['ngAnimate','ngRoute']);
+ var scApp = angular.module('soondcloodApp', ['ngAnimate','ngRoute','angular-loading-bar']);
+
+ scApp.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+   cfpLoadingBarProvider.latencyThreshold = 300;
+ }]);
 
  scApp.config(function ($routeProvider) {
   $routeProvider
@@ -24,8 +28,8 @@
     })
     .otherwise({
       redirectTo: '/'
-    });
   });
+});
 
 // Get List of followers of USERNAME
 scApp.factory('followerData', function($http) {
@@ -34,14 +38,14 @@ scApp.factory('followerData', function($http) {
   factory.userSearch = function(scDetails) {
       //return the promise directly.
       var username = scDetails.username || 'deepervibrations',
-          offset = scDetails.offset || '1',
-          limit = scDetails.tracksPerArtist || '10',
-          client_id = 'XXXX';
+          offset = scDetails.offset || '20',
+          limit = '200',
+          client_id = 'xxx';
 
       return $http.jsonp('http://api.soundcloud.com/users/' + username + '/followings.json', {
         params: {
           "client_id": client_id,
-          "limit":limit,
+          "limit": limit,
           "offset": offset,
           "callback": "JSON_CALLBACK"
         }
@@ -55,14 +59,15 @@ scApp.factory('followerData', function($http) {
 scApp.factory('trackData', function($http) {
   var factory = {};
 
-    factory.trackSearch = function(artist) {
+    factory.trackSearch = function(artist, tracksPerArtist) {
         //return the promise directly.
-        var limit = '1',
-        client_id = 'XXXX';
+        var limit = tracksPerArtist,
+            client_id = 'xxx';
+
         return $http.jsonp('http://api.soundcloud.com/users/' + artist + '/tracks.json', {
           params: {
             "client_id": client_id,
-            "limit":limit,
+            "limit": limit,
             "callback": "JSON_CALLBACK"
           }
         });
@@ -74,32 +79,41 @@ scApp.factory('trackData', function($http) {
 scApp.controller('followerSearch', function($scope, $location, $routeParams, followerData, trackData, $sce) {
   $scope.search = function() {
     $scope.tracks = null;
-    $scope.loading = true;
-    $scope.loading2 = true;
     followerData.userSearch($scope.scDetails)
     .then(function success(followers){
         var track,
             artist,
+            createDate,
+            created,
+            daysOld = $scope.scDetails.daysOld || 1,
+            today = new Date(),
+            tracksPerArtist = $scope.scDetails.tracksPerArtist || 2,
+            since = new Date(today.getFullYear(), today.getMonth(), today.getDate() - daysOld),
             tracks = [];
+
         followers.data.forEach(function(follower) {
           artist = follower.permalink;
-          trackData.trackSearch(artist)
+          trackData.trackSearch(artist, tracksPerArtist)
           .success(function success(trackobj) {
-            track = trackobj[0];
-              track.src = $sce.trustAsResourceUrl('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + track.id + '&amp;auto_play=false&amp;color=ff5500&amp;inverse=false&amp;show_user=true');
-              tracks.push(track);
-              $scope.loading = false;
+            if (trackobj.length) {
+              track = trackobj[0];
+              createDate = track.created_at;
+              created = new Date(createDate);
+              if (created > since) {
+                track.src = $sce.trustAsResourceUrl('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + track.id + '&amp;auto_play=false&amp;color=ff5500&amp;inverse=false&amp;show_user=true');
+                tracks.push(track);
+              } else {
+                console.log('Skipped track #',track.id, ' - older than ',daysOld,' days old.');
+              }
               $scope.tracks = tracks;
+            }
           });
         });
-        $scope.loading2 = false;
     }, function (response) {
       alert("check yo' username FOO!");
       // error handler
     });
   }
-// https://github.com/chieffancypants/angular-loading-bar
-
 
   // $scope.searchTerm = $location.$$path.split("/")[1];
   // if($scope.searchTerm!="") {
@@ -107,13 +121,47 @@ scApp.controller('followerSearch', function($scope, $location, $routeParams, fol
   // }
 });
 
-scApp.directive('scembed', function () {
-  return {
-    restrict: 'E',
-    replace: true,
-    template: '<iframe width="100%" height="20" scrolling="no" frameborder="no"></iframe>'
-    // link: function (scope, element, attributes) {
+scApp.directive('iframeLoad', ['$parse', function($parse) { // Inject $parse
+    return {
+        restrict: 'A',
+        link: function(scope, element, attr) {
+            var loadHandler = $parse(attr.iframeLoad); /* Parse value of
+                                                       'imgLoad' attribute */
+            // element.on('load', function(obj) {
+            //   var id = scope.$index + 1,
+            //       widgetIframe = document.getElementById('i_frame' + id),
+            //       widget = SC.Widget(widgetIframe);
+            //       console.log('iframe ready: ',widgetIframe);
+            //   widget.bind(SC.Widget.Events.READY, function() {
+            //     console.log('ready!!');
+            //     widget.bind(SC.Widget.Events.PLAY, function() {
+            //      console.log('playing');
+            //     });
+            //   });
+            // });
+        }
+    };
+}]);
+  // var linkFn = function(scope, element, attrs) {
+  //       alert('asd');
+  //       var widgetIframe = document.getElementById('i_frame'+id),
+  //           widget       = SC.Widget(widgetIframe);
+  //       widget.bind(SC.Widget.Events.READY, function() {
+  //       console.log('ready!!');
+  //       widget.bind(SC.Widget.Events.PLAY, function() {
+  //        console.log('playing');
+  //       });
+  //     });
 
-    // }
-  };
-});
+  //   //var loadHandler = $parse(attrs.id);
+  //                                       /* Parse value of
+  //                                        'imgLoad' attribute */
+  //   element.on('load', function() {
+  //       element.on('load', linkFn);
+  //       /* Run the function returned by $parse.
+  //       It needs the scope object to operate properly. */
+  //   });
+  //     // element.find('iframe').bind('load', function (event) {
+  //     //   console.log(event.target.contentWindow.document);
+  //     // });
+  // };
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of the soondcloodApp
  */
-scApp.controller('MainCtrl', function ($scope, SoundService, $sce) {
+scApp.controller('MainCtrl', function ($scope, SoundService, $sce, $timeout) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -24,7 +24,7 @@ scApp.controller('MainCtrl', function ($scope, SoundService, $sce) {
             results.data.forEach(function(result) {
                 result.src = $sce.trustAsResourceUrl('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + result.id + '&amp;auto_play=false&amp;color=ff5500&amp;inverse=false&amp;show_user=true');
             });
-            console.log('findtrks',results);
+
             $scope.results = results;
         });
     }
@@ -36,6 +36,23 @@ scApp.controller('MainCtrl', function ($scope, SoundService, $sce) {
     $scope.showTracks = function () {
         SoundService.getTracks($scope.scDetails);
         // $scope.scDetails = {};
+    }
+
+
+    $scope.onLoad = function() {
+     console.log('asdd');
+        // $timeout(alert('Image Loaded'));
+    }
+
+    $scope.checkReady = function(){
+        var widgetIframe = document.getElementById('i_frame'+id),
+            widget       = SC.Widget(widgetIframe);
+        widget.bind(SC.Widget.Events.READY, function() {
+        console.log('ready!!');
+        widget.bind(SC.Widget.Events.PLAY, function() {
+         console.log('playing');
+        });
+      });
     }
 
 });
