b645caeed26e8064df669973771c5959471192df
Oli Cundale <ocundale@gmail.com>
2014-09-03 17:40:47 +0100

    added panels

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -38,9 +38,9 @@ scApp.factory('followerData', function($http) {
   factory.userSearch = function(scDetails) {
       //return the promise directly.
       var username = scDetails.username || 'deepervibrations',
-          offset = scDetails.offset || '20',
-          limit = '200',
-          client_id = 'xxx';
+          offset = scDetails.offset || '0',
+          limit = scDetails.limit || '200',
+          client_id = 'ba852675bd10baf29350cac809711e18';
 
       return $http.jsonp('http://api.soundcloud.com/users/' + username + '/followings.json', {
         params: {
@@ -62,7 +62,7 @@ scApp.factory('trackData', function($http) {
     factory.trackSearch = function(artist, tracksPerArtist) {
         //return the promise directly.
         var limit = tracksPerArtist,
-            client_id = 'xxx';
+            client_id = 'ba852675bd10baf29350cac809711e18';
 
         return $http.jsonp('http://api.soundcloud.com/users/' + artist + '/tracks.json', {
           params: {
@@ -87,16 +87,19 @@ scApp.controller('followerSearch', function($scope, $location, $routeParams, fol
             created,
             daysOld = $scope.scDetails.daysOld || 1,
             today = new Date(),
-            tracksPerArtist = $scope.scDetails.tracksPerArtist || 2,
+            tracksPerArtist = $scope.scDetails.tracksPerArtist || 1,
             since = new Date(today.getFullYear(), today.getMonth(), today.getDate() - daysOld),
             tracks = [];
 
+        $scope.tracks = tracks;
+        //followers.data = all the followers (first 200)
         followers.data.forEach(function(follower) {
           artist = follower.permalink;
           trackData.trackSearch(artist, tracksPerArtist)
           .success(function success(trackobj) {
+            // trackobj = current track object
             if (trackobj.length) {
-              track = trackobj[0];
+              track = trackobj[0]; //get inside object
               createDate = track.created_at;
               created = new Date(createDate);
               if (created > since) {
@@ -105,29 +108,25 @@ scApp.controller('followerSearch', function($scope, $location, $routeParams, fol
               } else {
                 console.log('Skipped track #',track.id, ' - older than ',daysOld,' days old.');
               }
-              $scope.tracks = tracks;
             }
           });
         });
+
     }, function (response) {
       alert("check yo' username FOO!");
       // error handler
     });
-  }
-
-  // $scope.searchTerm = $location.$$path.split("/")[1];
-  // if($scope.searchTerm!="") {
-  //  $scope.search();
-  // }
+  };
 });
 
 scApp.directive('iframeLoad', ['$parse', function($parse) { // Inject $parse
     return {
         restrict: 'A',
         link: function(scope, element, attr) {
-            var loadHandler = $parse(attr.iframeLoad); /* Parse value of
-                                                       'imgLoad' attribute */
+            // var loadHandler = $parse(attr.iframeLoad); /* Parse value of
+            //                                            'imgLoad' attribute */
             // element.on('load', function(obj) {
+            //   console.log(obj);
             //   var id = scope.$index + 1,
             //       widgetIframe = document.getElementById('i_frame' + id),
             //       widget = SC.Widget(widgetIframe);
@@ -142,26 +141,3 @@ scApp.directive('iframeLoad', ['$parse', function($parse) { // Inject $parse
         }
     };
 }]);
-  // var linkFn = function(scope, element, attrs) {
-  //       alert('asd');
-  //       var widgetIframe = document.getElementById('i_frame'+id),
-  //           widget       = SC.Widget(widgetIframe);
-  //       widget.bind(SC.Widget.Events.READY, function() {
-  //       console.log('ready!!');
-  //       widget.bind(SC.Widget.Events.PLAY, function() {
-  //        console.log('playing');
-  //       });
-  //     });
-
-  //   //var loadHandler = $parse(attrs.id);
-  //                                       /* Parse value of
-  //                                        'imgLoad' attribute */
-  //   element.on('load', function() {
-  //       element.on('load', linkFn);
-  //       /* Run the function returned by $parse.
-  //       It needs the scope object to operate properly. */
-  //   });
-  //     // element.find('iframe').bind('load', function (event) {
-  //     //   console.log(event.target.contentWindow.document);
-  //     // });
-  // };
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,52 +7,22 @@
  * # MainCtrl
  * Controller of the soondcloodApp
  */
-scApp.controller('MainCtrl', function ($scope, SoundService, $sce, $timeout) {
+scApp.controller('MainCtrl', function ($scope, SoundService, $timeout) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    // $scope.getData = function () {
-    //     $scope.newData = DataService($scope.scDetails);
-    //     console.log(newData);
 
-    // };
-    $scope.findTracks = function() {
-        SoundService.displayTracks($scope.scDetails)
-        .then(function success(results){
-            results.data.forEach(function(result) {
-                result.src = $sce.trustAsResourceUrl('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + result.id + '&amp;auto_play=false&amp;color=ff5500&amp;inverse=false&amp;show_user=true');
-            });
-
-            $scope.results = results;
-        });
-    }
-
-    //scDetails:
-    // username
-    // tracksPerArtist
-    // daysOld
-    $scope.showTracks = function () {
-        SoundService.getTracks($scope.scDetails);
-        // $scope.scDetails = {};
-    }
-
-
-    $scope.onLoad = function() {
-     console.log('asdd');
-        // $timeout(alert('Image Loaded'));
-    }
-
-    $scope.checkReady = function(){
-        var widgetIframe = document.getElementById('i_frame'+id),
-            widget       = SC.Widget(widgetIframe);
-        widget.bind(SC.Widget.Events.READY, function() {
-        console.log('ready!!');
-        widget.bind(SC.Widget.Events.PLAY, function() {
-         console.log('playing');
-        });
-      });
-    }
+    $scope.checkReady = function() {
+      //   var widgetIframe = document.getElementById('i_frame'+id),
+      //       widget       = SC.Widget(widgetIframe);
+      //   widget.bind(SC.Widget.Events.READY, function() {
+      //   console.log('ready!!');
+      //   widget.bind(SC.Widget.Events.PLAY, function() {
+      //    console.log('playing');
+      //   });
+      // });
+    };
 
 });
